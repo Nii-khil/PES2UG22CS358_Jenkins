913b301a8bdfe3631685e0f84693f19130a1a055
Nikhil <111686634+Nii-khil@users.noreply.github.com>
2025-03-13 16:09:30 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello World, this is Nikhil Upadhyay PES2UG22CS358\n";
+  cout << "Hello World, this is Nikhil Upadhyay, PES2UG22CS358\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
